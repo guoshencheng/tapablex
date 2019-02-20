f5477eeab3fc81eb64539344b0cbae797ca47503
guoshencheng <648772021@qq.com>
2019-02-20 17:28:05 +0800

    add logger

--- src/SyncHook.ts
+++ src/SyncHook.ts
@@ -1,30 +1,44 @@
 import Hook from './Hook';
 import StageList from './utils/StageList';
+import Logger from './utils/Logger';
 
 export type SyncHookCallBack<T extends any[], R> = (...args: T) => R
 
 export type SyncHookTaps<T extends any[], R> = {
   fn: SyncHookCallBack<T, R>,
-  type: string,
   stage?: number,
-  name?: string,
+  name: string,
   before?: string | string[],
 }
 
 export type SyncHookTapOptions<T extends any[], R> = SyncHookTaps<T, R> & {
-  type?: string,
+  name?: string,
 }
 
 export default class SyncHook<T extends any[], R> extends Hook<T> {
 
   taps: StageList<SyncHookTaps<T, R>>
 
-  tap(type: string, options: SyncHookTapOptions<T, R>, fn: SyncHookCallBack<T, R>): void {
-    this._insert({
-      ...options,
-      type,
-      fn,
-    })
+  tap(name: string, fn: SyncHookCallBack<T, R>): void;
+  tap(name: string, options: SyncHookTapOptions<T, R>, fn: SyncHookCallBack<T, R>): void;
+  tap(name: string, options: SyncHookTapOptions<T, R> | SyncHookCallBack<T, R>, fn?: SyncHookCallBack<T, R>): void {
+    if (typeof options === 'function') {
+      options = {
+        fn: options,
+        name,
+      } as SyncHookTapOptions<T, R>;
+      this._insert(options)
+    } else {
+      if (fn) {
+        this._insert({
+          ...options, 
+          name,
+          fn,
+        })
+      } else {
+        Logger.warn('the third argument should be a function');
+      }
+    }
   }
 
   call(...args: T): R[] {
