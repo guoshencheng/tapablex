01c58ce344327f1ce81d8c7dedd78ef1f892519f
guoshencheng <648772021@qq.com>
2019-03-14 19:41:05 +0800

    add test for async hook call one by one

--- __tests__/AsyncHook.test.ts
+++ __tests__/AsyncHook.test.ts
@@ -20,4 +20,28 @@ describe('async hook call hook one by one in async', () => {
       expect(r).toEqual([RESULT2]);
     })
   })
+  it('hook can be call async successfully and one by one', () => {
+    const hook = new AsyncHook<any, string>();
+    const EVENT = 'EVENT';
+    const RESULT1 = 'RESULT1';
+    const RESULT2 = 'RESULT2';
+    const RESULT3 = 'RESULT3';
+    const sort = [] as string[]
+    hook.tap(EVENT, async () => {
+      sort.push('A')
+      return RESULT1;
+    })
+    hook.tap(EVENT, async () => {
+      sort.push('V')
+      return RESULT2;
+    })
+    hook.tap(EVENT, async () => {
+      sort.push('C')
+      return RESULT3;
+    })
+    hook.callAsync(EVENT, (_: any, r: string[]) => {
+      expect(sort).toEqual(['A', 'V', 'C']);
+      expect(r).toEqual([RESULT1, RESULT2, RESULT3]);
+    })
+  })
 });
